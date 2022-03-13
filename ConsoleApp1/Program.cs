7705640c172c25b5d6faae8a4a4f6fa287ee8df3
Anish <anish.arvind407@gmail.com>
2022-03-13 21:24:14 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more informati
+List<(string sizeName, int share)> shares = new List<(string size, int share)>();
+string input = string.Empty;
+
+
+do
+{
+    Console.WriteLine("Enter a Sample, S: 50.. PRESS c to exit");
+    input = Console.ReadLine() ?? throw new Exception("Nulls not allowed");
+    var data = input.Split(':');
+
+    if (!input.Equals("c"))
+        shares.Add((data[0], Int32.Parse(data[1])));
+
+} while (input != "c");
+
+if (shares.Sum(i => i.share) != 100)
+{
+    return;
+    Console.WriteLine("Sum is  not 100");
+}
+
+Console.WriteLine("Enter total quantities");
+double total = double.Parse(Console.ReadLine() ?? throw new Exception("Nulls not allowed"));
+
+//List<int> shares = new List<int>() { 25, 20, 10, 45 };
+
+void AdjustDistribution(SampleeDistributorModel result)
+{
+    result.roundedPercentage -= 1;
+}
+
+
+
+double calculatedTotal = 0;
+List<SampleeDistributorModel> result = new List<SampleeDistributorModel>();
+
+do
+{
+    result = shares.Select(x => SampleDistributor.CaclulateShare(total, x)).ToList();
+    calculatedTotal = result.Sum(i => i.roundedPercentage);
+
+    if (total == calculatedTotal) break;
+
+    if (calculatedTotal < total)
+    {
+        var obj =  result.Where(i => i.decimalPart < 0.5).OrderByDescending(i => i.decimalPart).FirstOrDefault();
+
+        obj.roundedPercentage += 1;
+    }
+    else
+    {
+        var obj = result.Where(i => i.decimalPart > 0.5).OrderByDescending(i => i.decimalPart).LastOrDefault();
+        obj.roundedPercentage -= 1;
+    }
+
+    calculatedTotal = result.Sum(i => i.roundedPercentage);
+
+} while (total != calculatedTotal);
+
+Console.WriteLine("--------- Size distributions are --------------");
+result.ForEach(i => Console.WriteLine($"Size Name:{i.sizeName}, Percentage: {i.roundedPercentage}"));
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("---------------------------------------------");
---
+++ ConsoleApp1/SizeDistributor.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more informati
+
+
+public static class SampleDistributor
+{
+    public static SampleeDistributorModel CaclulateShare(double total, (string sizeName, int share) percentage )
+    {
+        double percentageDistribution = (percentage.share* total) / 100;
+        double decimalPart2 = Math.Round(percentageDistribution, 2);
+        double decimalPart = Math.Round((decimalPart2 - Math.Truncate(decimalPart2)), 2);
+    
+        double roundedPercentage = Math.Round(decimalPart2, 0, MidpointRounding.AwayFromZero);
+
+        return new SampleeDistributorModel
+        {
+            decimalPart = decimalPart,
+            roundedPercentage = roundedPercentage,
+            percentageDistribution = percentageDistribution,
+            sizeName= percentage.sizeName
+        };
+    }
+}
+
+
+public class SampleeDistributorModel
+{
+    public double percentageDistribution { get; set; }
+    public double roundedPercentage { get; set; }
+    public double decimalPart { get; set; }
+    public string sizeName { get; set; }
+}
